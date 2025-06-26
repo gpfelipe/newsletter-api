b26c187a28e0272946a46443c5e48e1b9aa845be
gpfelipe <132168410+gpfelipe@users.noreply.github.com>
2025-06-26 15:39:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors()); // ✅ Libera requisições de outros domínios
 
 app.get('/news', (req, res) => {
   res.json([
